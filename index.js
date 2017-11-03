a7d6b75676f37039a830c2f341d7a0fa8e2badfc
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-03 16:01:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function add(a,b){
   return a + b;
-} 
+}
 
 function subtract(a,b){
   return a - b;
@@ -8,7 +8,7 @@ function subtract(a,b){
 
 function multiply(a,b){
   return a * b;
-} 
+}
 
 function divide(a,b){
   return a / b;
@@ -22,3 +22,10 @@ function dec(n){
   return --n;
 }
 
+function makeInt(n){
+  return parseInt(n,10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
